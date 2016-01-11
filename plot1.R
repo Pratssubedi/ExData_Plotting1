1ac7ff47dd2fa3f6e1cfb6c69ddb37e7afa54f91
unknown <i81245@prsubedi.d2hs.com>
2016-01-11 11:15:13 +0545

    Electric Power Consumption course Project 1

--- plot1.R
+++ plot1.R
@@ -2,4 +2,6 @@ filePath <- "household_power_consumption.txt"
 hpc <- read.table(filePath, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 dtHpc <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007") ,]
 globalActivePower <- as.numeric(dtHpc$Global_active_power)
+png("plot1.png", width=480, height=480)
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
--- plot2.R
+++ plot2.R
@@ -3,4 +3,6 @@ hpc <- read.table(filePath, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".
 dtHpc <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007") ,]
 datetime <- strptime(paste(dtHpc$Date, dtHpc$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(dtHpc$Global_active_power)
+png("plot2.png", width=480, height=480)
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
