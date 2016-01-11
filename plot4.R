37bd54db3279a9a20754f04bb4b2f9114e3f6f3a
Pratssubedi <subedi.pratss@gmail.com>
2016-01-11 13:06:12 +0545

    Updated a fix

--- plot4.R
+++ plot4.R
@@ -1,7 +1,7 @@
 filePath <- "household_power_consumption.txt"
 hpc <- read.table(filePath, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 dtHpc <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007") ,]
-datetime <- strptime(paste(dtHpc$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+datetime <- strptime(paste(dtHpc$Date, dtHpc$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(dtHpc$Global_active_power)
 globalReactivePower <- as.numeric(dtHpc$Global_reactive_power)
 voltage <- as.numeric(dtHpc$Voltage)
